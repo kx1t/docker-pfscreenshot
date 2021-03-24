724d4eede6cc2d2c4ae31d17bc63dc73f36ef3a8
kx1t <ramonkolb@gmail.com>
2021-03-24 12:25:05 -0400

    initial commit skeleton

---
+++ buildnow.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+set -x
+
+[[ "$1" != "-" ]] && BRANCH="$1"
+[[ "$BRANCH" == "-" ]] && BRANCH=dev
+
+[[ "$BRANCH" == "main" ]] && TAG="latest" || TAG="$BRANCH"
+
+# rebuild the container
+pushd ~/docker-pfscreenshot
+git checkout $BRANCH || exit 2
+
+git pull -a
+docker buildx build --compress --push $2 --platform linux/armhf,linux/arm64 --tag kx1t/planefence:$TAG .
+popd
